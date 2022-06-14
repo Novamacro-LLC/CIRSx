458ef866c5c96627a6ec38980bf66d07e84ba95c
Deepcast <Jdeepcast@gmail.com>
2022-06-14 18:05:02 -0500

    fixed a couple of things on tier welcome and video added html for bibliographies and research papers got the links working and page ready to go.

--- member/urls.py
+++ member/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
 path('tier_welcome/', views.tier_welcome, name='tier_welcome'),
-path('tier_videos/', views.tier_videos, name='tier_videos')
+path('tier_videos/', views.tier_videos, name='tier_videos'),
+path('bibliographies/', views.bibliographies, name='bibliographies'),
+path('research_papers/', views.research_papers, name='research_papers'),
+path('', include('home.urls'))
 
 ]
--- member/views.py
+++ member/views.py
@@ -7,3 +7,11 @@ def tier_welcome(request):
 
 def tier_videos(request):
     return render(request, 'member/tier_videos.html', {})
+
+
+def bibliographies(request):
+    return render(request, 'member/bibliographies.html', {})
+
+
+def research_papers(request):
+    return render(request, 'member/research_papers.html', {})
