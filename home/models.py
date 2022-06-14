6d32726575bea55a8b7640fafd68ef2fb059b15d
fbdav <brad.davison@novamacro.net>
2022-06-14 17:13:26 -0500

    admin and model changes

--- home/models.py
+++ home/models.py
@@ -5,9 +5,17 @@ from django.contrib.auth.models import User
 from django_currentuser.db.models import CurrentUserField
 
 
-class news(models.Model):
+class News(models.Model):
     title = models.CharField(max_length=150)
     body = models.TextField()
     image = models.CharField(max_length=175, default="Transparent-gold.jpg")
     date_added = models.DateTimeField(default=timezone.now)
     user = CurrentUserField()
+
+
+class Communication(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    subject = models.CharField(max_length=250)
+    message = models.TextField()
