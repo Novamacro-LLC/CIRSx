0664c2d1f92eeebae968b6b2c1477e3154e34aee
fbdav <brad.davison@novamacro.net>
2022-06-14 21:36:19 -0500

    updated database to postgres server

--- member/models.py
+++ member/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 
+class Event(models.Model):
+    event_name = models.CharField(max_length=150, null=False)
+    event_start_dt = models.DateField(null=False)
+    event_end_dt = models.DateField(null=False)
+    event_location = models.CharField(max_length=150, null=False)
+
+
+class Audience(models.Model):
+    aud_name = models.CharField(max_length=50, unique=True, null=False)
+    aud_desc = models.CharField(max_length=150, null=False)
+
+
+class DocumentType(models.Model):
+    doctyp_name = models.CharField(max_length=50, unique=True, null=False)
+    doctyp_desc = models.CharField(max_length=150, null=False)
+
+
+class Documents(models.Model):
+    title = models.CharField(max_length=200, null=False)
+
+
+
