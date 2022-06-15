382cb463f5f4404d12f6b1669813523f1c2c4c8d
Deepcast <Jdeepcast@gmail.com>
2022-06-14 19:39:19 -0500

    Fixed the pictures on About us

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import news
+from .models import News, Communication
 
 
-admin.site.register(news)
+admin.site.register(News)
+admin.site.register(Communication)
 
