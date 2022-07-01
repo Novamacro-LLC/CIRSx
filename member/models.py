4ba0074ee6c3ec69d676eb87758d5892f70d9a81
fbdav <brad.davison@novamacro.net>
2022-07-01 09:43:31 -0500

    changed event model name

--- member/models.py
+++ member/models.py
@@ -63,7 +63,7 @@ class Sponsor(models.Model):
     active = models.BooleanField()
 
 
-class Events(models.Model):
+class Event(models.Model):
     event_name = models.CharField(max_length=150, null=False)
     event_tag = models.CharField(max_length=250, null=False)
     event_start_date = models.DateField(null=False)
@@ -83,7 +83,7 @@ class EventOffer(models.Model):
     offer_title = models.CharField(max_length=250, null=False)
     offer_subtitle = models.CharField(max_length=250, null=True)
     offer_details = models.TextField(null=False)
-    event = models.ManyToManyField(Events)
+    event = models.ManyToManyField(Event)
     active = models.BooleanField()
 
 
@@ -97,7 +97,7 @@ class Document(models.Model):
     pub_dt = models.DateField(null=True)
     doc_abs = models.TextField(null=True)
     doc_txt = models.TextField(null=True)
-    event = models.ForeignKey(Events, null=True, on_delete=models.SET_NULL)
+    event = models.ForeignKey(Event, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.title
