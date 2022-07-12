0eb6a5f9666f738ff7f9c43f417f521bb65e473f
fbdav <brad.davison@novamacro.net>
2022-07-12 18:54:49 -0500

    added additional models

--- home/models.py
+++ home/models.py
@@ -29,4 +29,32 @@ class Communication(models.Model):
         return self.subject
 
 
+class Team(models.Model):
+    first_name = models.CharField(max_length=100, null=False)
+    last_name = models.CharField(max_length=100, null=False)
+    credentials = models.CharField(max_length=10, null=True)
+    founder = models.BooleanField()
+    title = models.CharField(max_length=50, null=False)
+    active = models.BooleanField()
 
+    def __str__(self):
+        return self.last_name + "," + self.first_name
+
+
+class StrategicPartner(models.Model):
+    first_name = models.CharField(max_length=100, null=False)
+    last_name = models.CharField(max_length=100, null=False)
+    credentials = models.CharField(max_length=10, null=True)
+    active = models.BooleanField()
+
+    def __str__(self):
+        return self.last_name + "," + self.first_name
+
+
+class SupportPartners(models.Model):
+    company_name = models.CharField(max_length=100, null=False)
+    logo = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
+    url = models.CharField(max_length=100, null=False)
+
+    def __str__(self):
+        return self.company_name
