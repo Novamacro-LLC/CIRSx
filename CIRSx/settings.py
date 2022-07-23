b984545529b59856f58f9e6e7c2c1d8f2b877fa4
fbdav <brad.davison@novamacro.net>
2022-07-22 21:10:20 -0500

    added new ip address

--- CIRSx/settings.py
+++ CIRSx/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3u3h!y9d^i=g+-+j-3qc4(0iav)20cd5)qce*yx0vmvf--n2h$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['cirsxtest.com', '10.136.66.156', 'localhost']
+ALLOWED_HOSTS = ['cirsxtest.com', '167.172.159.216', '10.136.66.156', 'localhost']
 
 
 # Application definition
