0743ada0ae2d46d4ec0abe53b370cafd866f783e
fbdav <brad.davison@novamacro.net>
2022-08-23 22:04:49 -0500

    updating settings

--- CIRSx/settings.py
+++ CIRSx/settings.py
@@ -133,7 +133,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+if not DEBUG:
+    STATIC_ROOT =''
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
