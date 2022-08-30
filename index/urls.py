4e4222510da8a29cd401df38d8a5d7878d59c88d
Deepcast <Jdeepcast@gmail.com>
2022-08-30 18:19:09 -0500

    fixed a couple of things in the code

--- index/urls.py
+++ index/urls.py
@@ -13,7 +13,6 @@ urlpatterns = [
     path('speakers/', views.speakers, name='speakers'),
     path('contact_us/', views.contact_us, name='contact_us'),
     path('shoey/', views.shoey, name='shoey'),
-    path('test/', views.test, name='test'),
     path('stripe_pro/', views.stripe_pro, name='stripe_pro'),
     path('stripe_patient/', views.stripe_patient, name='stripe_patient'),
     path('stripe_events/', views.stripe_events, name='stripe_events'),
