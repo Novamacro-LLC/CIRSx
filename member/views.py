28eb41e638cdf2dff272a19c57a0bba81143b437
Deepcast <Jdeepcast@gmail.com>
2022-09-15 21:32:21 -0500

    worked on the base templates

--- member/views.py
+++ member/views.py
@@ -8,14 +8,14 @@ from index.views import active_events
 @login_required()
 def tier_welcome(request):
     event = active_events()
-    context = {'event': event}
+    context = {'event': event, 'event_info': event_info}
     return render(request, 'member/tier_welcome.html', context)
 
 
 @login_required()
 def tier_videos(request):
     event = active_events()
-    context = {'event': event}
+    context = {'event': event, 'event_info': event_info}
     return render(request, 'member/tier_videos.html', context)
 
 
@@ -23,7 +23,7 @@ def tier_videos(request):
 def bibliographies(request):
     bib = Document.objects.filter(doctyp_num=1).order_by('title')
     event = active_events()
-    context = {'event': event, 'bib': bib}
+    context = {'event': event, 'bib': bib, 'event_info': event_info}
     return render(request, 'member/bibliographies.html', context)
 
 
@@ -31,33 +31,33 @@ def bibliographies(request):
 def research_papers(request):
     rsh = Document.objects.filter(doctyp_num=2).order_by('title')
     event = active_events()
-    context = {'event': event, 'rsh': rsh}
+    context = {'event': event, 'rsh': rsh, 'event_info': event_info}
     return render(request, 'member/research_papers.html', context)
 
 
 @login_required()
 def archived_events(request):
     event = active_events()
-    context = {'event': event}
+    context = {'event': event, 'event_info': event_info}
     return render(request, 'member/archived_events.html', context)
 
 
 @login_required()
 def deeper_dive(request):
     event = active_events()
-    context = {'event': event}
+    context = {'event': event, 'event_info': event_info}
     return render(request, 'member/deeper_dive.html', context)
 
 
 @login_required()
 def hopkinton(request):
     event = active_events()
-    context = {'event': event}
+    context = {'event': event, 'event_info': event_info}
     return render(request, 'member/hopkinton.html', context)
 
 
 @login_required()
 def podcasts(request):
     event = active_events()
-    context = {'event': event}
+    context = {'event': event, 'event_info': event_info}
     return render(request, 'member/podcasts.html', context)
