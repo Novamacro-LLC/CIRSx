688110d8cad4f16444e3d43b647b8c9ae9ead03c
Deepcast <Jdeepcast@gmail.com>
2022-10-10 14:35:19 -0500

    worked on the doc route

--- member/admin.py
+++ member/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Audience, DocumentType, Document
+from .models import Audience, DocumentType, Document, DocumentRoute
 
 admin.site.register(Audience)
 admin.site.register(DocumentType)
 admin.site.register(Document)
+admin.site.register(DocumentRoute)
 
 
 class DocumentTypeAdmin(admin.ModelAdmin):
