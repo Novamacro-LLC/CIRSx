ae43c4f3cb419e6e5b094c433cfa78a757068590
fbdav <brad.davison@novamacro.net>
2022-10-11 17:23:22 -0500

    updated Member Registration

--- event/views.py
+++ event/views.py
@@ -5,30 +5,25 @@ from .models import EventRegistration, Guest
 from django.forms import modelformset_factory
 from django.contrib.auth.decorators import login_required
 from django.db import transaction, IntegrityError
-from index.views import active_events
+from index.views import active_events, droute
 
 
 @login_required()
 def events_register(request):
     base_template_name = 'member/base.html'
     event = active_events()
+    dr = droute()
     user = request.user.id
-    context = {'base_template_name': base_template_name, 'event': event, 'user':user}
+    context = {'base_template_name': base_template_name, 'event': event, 'user': user, 'dr': dr}
     if request.method == 'POST':
         form = EventRegisterForm(data=request.POST)
         print(form.data.get('event'))
-        formset = GuestFormSet(data=request.POST)
-        if formset.is_valid() and form.is_valid():
+        if form.is_valid():
             form.save()
-            event_reg = EventRegistration.objects.filter(event=form.data.get('event'), member=form.data.get('member'), attendance=form.data.get('attendance')).last()
-            print(event_reg.id)
-            #add event_reg to guest and input into database
-            formset.save()
             return render(request, 'index/stripe_events.html', context)
         else:
             HttpResponse('Form did not go through, there was an error.')
     else:
         form = EventRegisterForm()
-        formset = GuestFormSet(queryset=Guest.objects.none())
-        context = {'formset': formset, 'form': form, 'base_template_name': base_template_name, 'event': event}
+        context = {'form': form, 'base_template_name': base_template_name, 'event': event, 'dr': dr, 'user': user}
         return render(request, 'event/events_register.html', context)
